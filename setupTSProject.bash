3af02b0e0ac1e513b6de0f8715915012ab9e84fd
Simen de Lange <simen.de.lange@gmail.com>
2020-09-26 16:28:56 +0200

    Script for setting up a TS project

---
+++ setupTSProject.bash
@@ -0,0 +1,33 @@
+#/bin/bash
+
+npm init
+
+npm install -D @types/node @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier nodemon prettier rimraf typescript @types/jest jest
+
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/.prettierrc.json
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/.eslintrc.json
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/.gitignore
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/tsconfig.json
+
+node -e "
+const fs = require(\"fs\");
+const package = require(\"./package.json\");
+
+package.scripts = {
+	\"build\": \"tsc\",
+	\"build:dev\": \"tsc --watch --preserveWatchOutput\",
+	\"start\": \"NODE_ENV=\\\"production\\\" node dist/app\",
+	\"start:dev\": \"NODE_ENV=\\\"development\\\" node dist/app\",
+	\"test\": \"jest\",
+	\"test:dev\": \"jest --watchAll\",
+	\"lint\": \"eslint src/**\",
+	\"clean\": \"rimraf dist\"
+}
+
+fs.writeFileSync(\"./package.json\", JSON.stringify(package, null, 2));
+"
+
+mkdir src
+
+touch src/app.ts
+touch src/app.test.ts
