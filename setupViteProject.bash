38014969180510d026c810b2125dc0bcfe577340
Simen de Lange <simen.de.lange@gmail.com>
2022-07-04 08:13:43 +0200

    Script for setting up av vite project

---
+++ setupViteProject.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+npm create vite@latest .
+
+npm install
+npm install -D @typescript-eslint/eslint-plugin @typescript-eslint/parser eslint eslint-config-prettier eslint-plugin-prettier prettier @types/jest jest ts-jest
+
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/.prettierrc.json
+wget https://raw.githubusercontent.com/Suppen/dotfiles/master/.eslintrc.json
+
+node -e "
+const fs = require(\"fs\");
+const package = require(\"./package.json\");
+
+package.scripts = {
+	\"test\": \"jest\",
+	\"test:dev\": \"jest --watchAll\",
+	\"lint\": \"eslint src/**\"
+};
+
+package.jest = {
+	\"preset\": \"ts-jest\",
+	\"testEnvironment\": \"node\",
+	\"rootDir\": \"src/\"
+};
+
+fs.writeFileSync(\"./package.json\", JSON.stringify(package, null, 2));
+"
