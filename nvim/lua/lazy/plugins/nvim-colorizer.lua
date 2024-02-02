024e4ec0a408e740df984d32425f2a75c73bd503
Simen de Lange <simen.de.lange@gmail.com>
2024-02-02 21:43:09 +0100

    nvim-colorizer

---
+++ nvim/lua/lazy/plugins/nvim-colorizer.lua
@@ -0,0 +1,27 @@
+-- Color highlighter
+
+local filetypes = {
+	'css', 'scss', 'sass', 'less'
+}
+
+return {
+	'NvChad/nvim-colorizer.lua',
+	ft = filetypes,
+	opts = {
+		filetypes = filetypes,
+		user_default_options = {
+			css = true,     -- Enable all CSS features: rgb_fn, hsl_fn, names, RGB, RRGGBB
+			css_fn = true,  -- Enable all CSS *functions*: rgb_fn, hsl_fn
+			-- Available modes for `mode`: foreground, background,  virtualtext
+			mode = "background", -- Set the display mode.
+			-- Available methods are false / true / "normal" / "lsp" / "both"
+			-- True is same as normal
+
+			-- parsers can contain values used in |user_default_options|
+			sass = { enable = false, parsers = { "css" }, }, -- Enable sass colors
+			virtualtext = "■",
+		},
+		-- all the sub-options of filetypes apply to buftypes
+		--buftypes = {},
+	}
+}
