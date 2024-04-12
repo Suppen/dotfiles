72e39d5275292c02b758ba24c3ee58c3294c5a4f
Simen de Lange <simendl@proton.me>
2024-04-12 18:45:55 +0200

    Slight changes to autocompletion

--- nvim/lua/lazy/plugins/LuaSnip.lua
+++ nvim/lua/lazy/plugins/LuaSnip.lua
@@ -1,25 +1,26 @@
 -- Snippet engine
 
 return {
-	'L3MON4D3/LuaSnip',
-	version = 'v2.*',
-	build = 'make install_jsregexp',
+	"L3MON4D3/LuaSnip",
+	version = "v2.*",
+	build = "make install_jsregexp",
 	lazy = true,
 	dependencies = {
-		'rafamadriz/friendly-snippets',            -- Snippets
-		'johnpapa/vscode-angular-snippets',        -- Angular snippets
-		'dsznajder/vscode-es7-javascript-react-snippets', -- React snippets
+		"saadparwaiz1/cmp_luasnip",                 -- Autocomplete from snippets
+		"rafamadriz/friendly-snippets",             -- Snippets
+		"johnpapa/vscode-angular-snippets",         -- Angular snippets
+		"dsznajder/vscode-es7-javascript-react-snippets", -- React snippets
 	},
 	config = function()
-		local datapath = vim.fn.stdpath('data')
+		local datapath = vim.fn.stdpath("data")
 
-		local luasnip = require('luasnip.loaders.from_vscode')
+		local luasnip = require("luasnip.loaders.from_vscode")
 		luasnip.lazy_load({
 			paths = {
-				datapath .. '/lazy/friendly-snippets',
-				datapath .. '/lazy/vscode-angular-snippets',
-				datapath .. '/lazy/vscode-es7-javascript-react-snippets',
-			}
+				datapath .. "/lazy/friendly-snippets",
+				datapath .. "/lazy/vscode-angular-snippets",
+				datapath .. "/lazy/vscode-es7-javascript-react-snippets",
+			},
 		})
-	end
+	end,
 }
--- nvim/lua/lazy/plugins/nvim-cmp.lua
+++ nvim/lua/lazy/plugins/nvim-cmp.lua
@@ -1,61 +1,60 @@
 -- Autocomplete engine
 
 return {
-	'hrsh7th/nvim-cmp',
+	"hrsh7th/nvim-cmp",
 	event = "InsertEnter",
 	dependencies = {
-		'hrsh7th/cmp-buffer',                      -- Autocomplete from buffer
-		'hrsh7th/cmp-nvim-lsp-signature-help',     -- Show signature help
-		'hrsh7th/cmp-nvim-lsp',                    -- Autocomplete from LSP
-		'hrsh7th/cmp-nvim-lua',                    -- Autocomplete from Lua
-		'hrsh7th/cmp-path',                        -- Autocomplete from path
-		'L3MON4D3/LuaSnip',
-		'saadparwaiz1/cmp_luasnip',                -- Autocomplete from snippets
+		"hrsh7th/cmp-buffer",            -- Autocomplete from buffer
+		"hrsh7th/cmp-nvim-lsp-signature-help", -- Show signature help
+		"hrsh7th/cmp-nvim-lsp",          -- Autocomplete from LSP
+		"hrsh7th/cmp-nvim-lua",          -- Autocomplete from Lua
+		"hrsh7th/cmp-path",              -- Autocomplete from path
+		"L3MON4D3/LuaSnip",
 	},
 	config = function()
-		local cmp = require('cmp')
+		local cmp = require("cmp")
 
-		cmp.setup {
-			mapping = cmp.mapping.preset.insert {
-				['<C-b>'] = cmp.mapping.scroll_docs(-4),
-				['<C-f>'] = cmp.mapping.scroll_docs(4),
-				['<C-Space>'] = cmp.mapping.complete(),
-				['<C-e>'] = cmp.mapping.abort(),
-				['<CR>'] = cmp.mapping.confirm({ select = false })
-			},
+		cmp.setup({
+			mapping = cmp.mapping.preset.insert({
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+			}),
 			snippet = {
 				expand = function(args)
 					require("luasnip").lsp_expand(args.body)
-				end
+				end,
 			},
 			window = {
 				completion = cmp.config.window.bordered(),
 				documentation = cmp.config.window.bordered(),
 			},
 			sources = cmp.config.sources({
-				{ name = 'nvim_lsp' },
-				{ name = 'nvim_lua' },
-				{ name = 'nvim_lsp_signature_help' },
-				{ name = 'path' },
-				{ name = 'buffer' },
-				{ name = 'luasnip' }
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lua" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "path" },
+				{ name = "buffer" },
+				{ name = "luasnip" },
 			}, {
-				{ name = 'buffer' }
+				{ name = "buffer" },
 			}),
 			formatting = {
-				fields = { 'menu', 'abbr', 'kind' },
+				fields = { "menu", "abbr", "kind" },
 				format = function(entry, item)
 					local menu_icon = {
-						nvim_lsp = 'λ',
-						luasnip = '⋗',
-						buffer = 'Ω',
-						path = '🖫',
+						nvim_lsp = "λ",
+						luasnip = "⋗",
+						buffer = "Ω",
+						path = "🖫",
 					}
 					item.menu = menu_icon[entry.source.name]
 					return item
 				end,
 			},
-		}
+		})
 
 		--cmp.setup.cmdline({ '/', '?' }, {
 		--mapping = cmp.mapping.preset.cmdline(),
@@ -72,5 +71,5 @@ return {
 		--{ name = 'cmdline' }
 		--})
 		--})
-	end
+	end,
 }
