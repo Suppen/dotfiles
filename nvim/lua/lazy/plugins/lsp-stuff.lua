74bef05360c2dd5bcfa268e6fe6696c057bb71c6
Simen de Lange <simendl@proton.me>
2024-04-13 23:03:22 +0200

    Moved keymaps

--- nvim/lua/lazy/plugins/lsp-stuff.lua
+++ nvim/lua/lazy/plugins/lsp-stuff.lua
@@ -1,5 +1,56 @@
 -- LSP stuff
 
+--------------
+-- Mappings --
+--------------
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+	group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+	callback = function(ev)
+		-- Enable inlay hints
+		-- XXX Disabled due to bugs. It is as of now an experimental feature, so bugs are not very surprising
+		--local client = vim.lsp.get_client_by_id(ev.data.client_id)
+		--if client.server_capabilities.inlayHintProvider then
+		--vim.lsp.inlay_hint.enable(ev.buf, true)
+		--end
+
+		-- Buffer local mappings.
+		-- See `:help vim.lsp.*` for documentation on any of the below functions
+		local opts = { noremap = true, silent = true, buffer = ev.buf }
+		--vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+		vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
+		vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+		vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+		vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+		vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+		vim.keymap.set("n", "<leader>wl", function()
+			print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+		end, opts)
+		vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
+
+		-- Format on save
+		if vim.b[ev.buf] == nil or vim.b[ev.buf].format_on_save_set == nil then
+			-- Guard against setting the autocmd multiple times
+			vim.b[ev.buf] = vim.b[ev.buf] or {}
+			vim.b[ev.buf].format_on_save_set = true -- Set the flag to true
+
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				buffer = ev.buf,
+				callback = function()
+					vim.lsp.buf.format({ async = false })
+				end,
+			})
+		end
+	end,
+})
+
 return {
 	-- Mason LSP manager
 	{
@@ -137,53 +188,6 @@ return {
 
 			lspconfig.hls.setup({
 				capabilities = capabilities,
-			})
-
-			--------------
-			-- Mappings --
-			--------------
-
-			-- Use LspAttach autocommand to only map the following keys
-			-- after the language server attaches to the current buffer
-			vim.api.nvim_create_autocmd("LspAttach", {
-				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-				callback = function(ev)
-					-- Enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-
-					-- Buffer local mappings.
-					-- See `:help vim.lsp.*` for documentation on any of the below functions
-					local opts = { noremap = true, silent = true, buffer = ev.buf }
-					--vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-					vim.keymap.set("n", "gy", vim.lsp.buf.type_definition, opts)
-					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
-					vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
-					vim.keymap.set("n", "<leader>wl", function()
-						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-					end, opts)
-					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-					vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-					vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, opts)
-
-					-- Format on save
-					if vim.b[ev.buf] == nil or vim.b[ev.buf].format_on_save_set == nil then
-						-- Guard against setting the autocmd multiple times
-						vim.b[ev.buf] = vim.b[ev.buf] or {}
-						vim.b[ev.buf].format_on_save_set = true -- Set the flag to true
-
-						vim.api.nvim_create_autocmd("BufWritePre", {
-							buffer = ev.buf,
-							callback = function()
-								vim.lsp.buf.format({ async = false })
-							end,
-						})
-					end
-				end,
 			})
 		end,
 	},
