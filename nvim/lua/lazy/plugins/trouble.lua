81a351146e3d05fa7d307480ea168437631f42ca
Simen de Lange <simendl@proton.me>
2024-06-22 10:09:06 +0200

    Updated Trouble, and removed outline
    Trouble now has an outline (document symbols) feature

--- nvim/lua/lazy/plugins/trouble.lua
+++ nvim/lua/lazy/plugins/trouble.lua
@@ -2,25 +2,41 @@
 
 return {
 	"folke/trouble.nvim",
+	opts = {},
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	config = function()
-		vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", { silent = true, noremap = true })
-		vim.keymap.set(
-			"n",
-			"<leader>xw",
-			"<cmd>TroubleToggle workspace_diagnostics<cr>",
-			{ silent = true, noremap = true }
-		)
-		vim.keymap.set(
-			"n",
-			"<leader>xd",
-			"<cmd>TroubleToggle document_diagnostics<cr>",
-			{ silent = true, noremap = true }
-		)
-		vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", { silent = true, noremap = true })
-		vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", { silent = true, noremap = true })
-		vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", { silent = true, noremap = true })
-	end,
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>o",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
+	},
 }
