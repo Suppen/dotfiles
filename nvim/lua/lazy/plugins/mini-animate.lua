5f16980d06566101f0200ff56f7389607099be60
Simen de Lange <simendl@proton.me>
2024-06-25 14:42:01 +0200

    Command to activate animate.mini

--- nvim/lua/lazy/plugins/mini-animate.lua
+++ nvim/lua/lazy/plugins/mini-animate.lua
@@ -1,8 +1,12 @@
 -- Animations of cursor movement and window operations
 
+vim.api.nvim_create_user_command("Animate", function()
+	require("lazy").load({ plugins = { "mini.animate" } })
+end, {})
+
 return {
 	"echasnovski/mini.animate",
-	version = false,
-	enabled = false,
+	lazy = true,
+	command = "Animate",
 	opts = {},
 }
