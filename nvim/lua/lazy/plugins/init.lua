00b1e41e3b5e147fedc68a93fcb545d7bc6acb87
Simen de Lange <simendl@proton.me>
2025-06-27 16:12:24 +0200

    Disabled stuff and added treesitter context

--- nvim/lua/lazy/plugins/init.lua
+++ nvim/lua/lazy/plugins/init.lua
@@ -1,6 +1,5 @@
 return {
-	"tpope/vim-repeat", -- Repeat plugin commands
+	-- "tpope/vim-repeat", -- Repeat plugin commands
 	"tpope/vim-fugitive", -- Git commands
-	"nvim-tree/nvim-web-devicons", -- Fancy icons
 	"RRethy/vim-illuminate", -- Highlight all instances of the word under the cursor
 }
--- nvim/lua/lazy/plugins/nvim-treesitter.lua
+++ nvim/lua/lazy/plugins/nvim-treesitter.lua
@@ -1,38 +1,48 @@
 -- Syntax highlighting
 
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
-	opts = {
-		sync_install = false,
-		auto_install = true,
-		ensure_installed = {
-			"typescript",
-			"javascript",
-			"tsx",
-			"json",
-			"html",
-			"css",
-			"scss",
-			"lua",
-			"rust",
-			"toml",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		branch = "main",
+		build = ":TSUpdate",
+		opts = {
+			sync_install = false,
+			auto_install = true,
+			ensure_installed = {
+				"typescript",
+				"javascript",
+				"tsx",
+				"json",
+				"html",
+				"css",
+				"scss",
+				"lua",
+				"rust",
+				"toml",
+			},
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			ident = {
+				enable = true,
+			},
+			tsx = {
+				enable = true,
+			},
+			autopairs = {
+				enable = true,
+			},
+			autotag = {
+				enable = true,
+			},
 		},
-		highlight = {
-			enable = true,
-			additional_vim_regex_highlighting = false,
-		},
-		ident = {
-			enable = true,
-		},
-		tsx = {
-			enable = true,
-		},
-		autopairs = {
-			enable = true,
-		},
-		autotag = {
-			enable = true,
-		},
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		dependencies = { "nvim-treesitter/nvim-treesitter" },
+		config = function()
+			require("treesitter-context").setup()
+		end,
 	},
 }
